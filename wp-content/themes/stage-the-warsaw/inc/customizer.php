d3e8afe0d4935b30a0c5531e790b7ebfafe1ae70
ArturDr <robek.artur@gmail.com>
2024-01-25 13:05:21 +0100

    first commit

---
+++ wp-content/themes/stage-the-warsaw/inc/customizer.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Stage The Warsaw Theme Customizer
+ *
+ * @package Stage_The_Warsaw
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function stage_the_warsaw_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+
+	if ( isset( $wp_customize->selective_refresh ) ) {
+		$wp_customize->selective_refresh->add_partial(
+			'blogname',
+			array(
+				'selector'        => '.site-title a',
+				'render_callback' => 'stage_the_warsaw_customize_partial_blogname',
+			)
+		);
+		$wp_customize->selective_refresh->add_partial(
+			'blogdescription',
+			array(
+				'selector'        => '.site-description',
+				'render_callback' => 'stage_the_warsaw_customize_partial_blogdescription',
+			)
+		);
+	}
+}
+add_action( 'customize_register', 'stage_the_warsaw_customize_register' );
+
+/**
+ * Render the site title for the selective refresh partial.
+ *
+ * @return void
+ */
+function stage_the_warsaw_customize_partial_blogname() {
+	bloginfo( 'name' );
+}
+
+/**
+ * Render the site tagline for the selective refresh partial.
+ *
+ * @return void
+ */
+function stage_the_warsaw_customize_partial_blogdescription() {
+	bloginfo( 'description' );
+}
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function stage_the_warsaw_customize_preview_js() {
+	wp_enqueue_script( 'stage-the-warsaw-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), _S_VERSION, true );
+}
+add_action( 'customize_preview_init', 'stage_the_warsaw_customize_preview_js' );
